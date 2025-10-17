184ce3aa5b9c9d3e79680cfd1289bad91c2af291
Matrix <matrix120206a@gmail.com>
2025-10-18 06:14:04 +0700

    update lagi

---
+++ Scripts/SetDarkThemeCMD.bat
@@ -0,0 +1,5 @@
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v AppsUseLightTheme /t REG_DWORD /d 0 /f
+
+reg add "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" /v SystemUsesLightTheme /t REG_DWORD /d 0 /f
+
+taskkill /f /im explorer.exe & start explorer.exe
