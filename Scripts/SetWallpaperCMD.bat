58b7c7950fe96d7045f5d82193b167bb5ec41097
Matrix <matrix120206a@gmail.com>
2025-10-18 10:17:45 +0700

    update.bat

--- Scripts/SetWallpaperCMD.bat
+++ Scripts/SetWallpaperCMD.bat
@@ -1,13 +1,13 @@
 @echo off
 echo Mengubah wallpaper menggunakan RUNDLL32...
 
-# Set wallpaper lewat registry
+rem Set wallpaper lewat registry
 reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "C:\testw\Scripts\windows-11-dark-mode.png" /f
 
-# Refresh wallpaper
+rem Refresh wallpaper
 RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
 
-# Jika perlu, tambahkan delay supaya wallpaper diterapkan sebelum lanjut
+rem Jika perlu, tambahkan delay supaya wallpaper diterapkan sebelum lanjut
 timeout /t 5 /nobreak >nul
 
 echo Wallpaper berhasil diubah.
